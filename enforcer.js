9d2050d6e2f18e6178437fa84b77c57bcc554438
Ben Ghaem <ben.ghaem@gmail.com>
2015-07-12 21:21:40 -0400

    large changes to dynamic keys and global merging

--- enforcer.js
+++ enforcer.js
@@ -21,7 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-var OBJ_REQ_KEYS = ['keys','require']
+var OBJ_REQ_KEYS = ['keys','require','type']
 var OBJ_KEYS = ['keys','require','type']
 
 var enforcer = function(){
@@ -58,8 +58,10 @@ function find_missing(key_arr, test_arr){
 	var arr_b = test_arr;
 	var miss = [];
 	for (var i = arr_a.length - 1; i >= 0; i--) {
-		if (arr_b.indexOf(arr_a[i]) === -1) {
+		if (arr_a[i] !== '*'){
+			if (arr_b.indexOf(arr_a[i]) === -1) {
 			miss.push(arr_a[i])
+			};
 		};
 	};
 	return miss;
@@ -75,7 +77,8 @@ function find_diff(key_arr, test_arr){
 	return {'missing':find_missing(key_arr,test_arr),'extra':find_extra(key_arr,test_arr)};
 }
 
-function top_level(protocol){
+function top_level(incoming_protocol){
+	var protocol = incoming_protocol
 	name = protocol['name'];
 	style = protocol['style']
 	if (style === 'json') {
@@ -83,55 +86,69 @@ function top_level(protocol){
 	};
 }
 
+
+//various operations to remove macro aspects (globals) from protocol definition
 function parse_json_top(protocol){
 	//make nice var name
 	var p = protocol
 
-	return parse_json_create_parser(p)
-	// var p_req_k = p['require']
-	// var p_K = p['keys']
+	if (p['type']){
+		if (p.type === "object"){
+			//test req keys
+			enforcer.ck_keys(p,OBJ_REQ_KEYS,["*"])
 
-	// //Check for required keys
-	// if (enforcer.ck_keys(p,p_req_k,p_K)){
-		
-	// 	for (var i = Object.keys(p).length - 1; i >= 0; i--) {
-	// 		if (Object.keys(p)[i] !==  "#" ){
-	// 			// key_extended = parse_json_merge_global(Object.keys(p)[i],p['#'])
-	// 			var parser = parse_json_create_parser(key_extended)
-	// 			return parser;
-	// 		}
-	// 	};
+			p = parse_json_eval_globals(p)
 
-		// var req = p['request']
-		// var res = p['response']
+			// p = parse_json_eval_runtime_functions(p)
 
-		// if (p['#']){
-		// 	req = parse_json_merge_global(req,p['global']);
-		// 	res = parse_json_merge_global(res,p['global']);
-		// }
-		
-		// var req_parser = parse_json_create_parser(req);
-		// return req_parser
+			// remove possible top level global:
 
-	// }
+			if (p['#']){
+				delete p['#']
+			}
+
+			return parse_json_create_parser(p)
+		}
+		if (p.type === "string"){
+			return function(x){
+				return (typeof(x) === "string")
+			}
+		}
+	}
+
+	
+
 }
 
 
 function parse_json_eval_globals(obj){
 	if (obj['type']){
 		if (obj['type'] === "object") {
-			var obj_keys = obj['keys'].map(convert_key).concat("#");
+			var extra = []
+			if (obj['#']){
+				extra = ["#"]
+			}
+			var obj_keys = obj['keys'].map(convert_key).concat(extra)
+			if (obj_keys.length === 1 && obj_keys[0] === "*"){
+				return obj;
+			}
+			console.log(obj_keys)
 			for (var i = obj_keys.length - 1; i >= 0; i--) {
-				if (obj[obj_keys[i]]['#']){
-					return merge_vals(obj,parse_json_eval_globals(obj['#']))
-				} 
-				else if (obj['#']){
-					return merge_vals(obj,obj['#']);
-				}
-				else{
-					return obj;
+				if (obj_keys[i] !== '*'){
+					if (obj[obj_keys[i]]['#']){
+						return merge_globals(obj,parse_json_eval_globals(obj[obj_keys[i]]))
+					} 
+					else if (obj['#']){
+						var cache_obj = obj['#']
+						delete obj['#']
+						return merge_globals(obj,cache_obj);
+					}
+					else{
+						return obj;
+					}
 				}
 			};
+
 		}
 	}
 }
@@ -165,126 +182,139 @@ function convert_key(key){
 		out = key;
 	}
 	else{
-		out = "_"+key
+		out = "_"+key;
 	}
 	return out;
 }
 
-function parse_json_create_parser(protocol_obj){
-	var fn_bank = {}
 
-	parse_json_eval_globals(protocol_obj)
+function parse_json_create_parser(protocol_obj){
+	var dyn_bank = {}
+	var proto_keys;
+	var proto_reqs;
+	var working_keys;
 
 
 	console.log(protocol_obj)
-
-	console.log('Test deep dive')
-	deep_dive_obj(protocol_obj,'top')
-	console.log(fn_bank)
-
-
 
 	function derive_optional(key,keys,reqs){
 		return keys.indexOf(key) !== -1 && reqs.indexOf(key) === -1
 	}
 
-	//discover all non object keys and run 
-	function deep_dive_obj(obj,name){
-		//check for type atribute
-		if (obj['type']){
-			//if the type is object there may be some keys
-			if (obj['type'] === 'object'){
 
-				//grab those keys then check for global marker
-				var test_keys = obj['keys'].map(convert_key);
-
-				//there may be a global marker
-				if (obj['#']){
-					deep_dive_obj(obj['#'])
-				}
-				console.log(obj)
-
-
-				
-				//iterate over test keys
-				for (var i = test_keys.length - 1; i >= 0; i--) {
-					//if we found a key --> attempt to deep dive it
-					if (obj[test_keys[i]]){
-						return deep_dive_obj(obj[test_keys[i]],name + "-" +obj['keys'][i]);
-					}
-				};
-			}
-			if (obj['type'] === 'array'){
-				console.log('found array')
-				if (obj['map2key']){
-					console.log('found map2key')
-					var newfn = function(val){val.map(obj['map2key'])}
-					fn_bank[name] = newfn
-				}
-			}
-			else{
-				console.log(obj)
-				return;
-			}
-		}
+	function append_reqs(arr){
+		proto_reqs = proto_reqs.concat(arr)
 	}
 
-	var proto_keys = protocol_obj['keys']
-	var proto_reqs = protocol_obj['require']
+	function merge_dyn_keys(arr, parser){
+		proto_keys = proto_keys.concat(arr)
+		working_keys = proto_keys.map(convert_key)
+		var converted_arr = arr.map(convert_key)
+		for (var i = converted_arr.length - 1; i >= 0; i--) {
+			dyn_bank[converted_arr[i]] = {}
+			dyn_bank[converted_arr[i]]['parser'] = parser
+			dyn_bank[converted_arr[i]]['type'] = "dyn_key"
+		};
+		
+	}
 
-	var working_keys = protocol_obj['keys'].map(convert_key)
+
+	proto_keys = protocol_obj['keys']
+	proto_reqs = protocol_obj['require']
+
+	working_keys = protocol_obj['keys'].map(convert_key)
 	if (working_keys.length === 1 && working_keys[0] === "*"){
 		working_keys = [];
 	}
 
-	if (enforcer.ck_keys(protocol_obj,OBJ_REQ_KEYS.concat(working_keys),OBJ_KEYS.concat(working_keys))){
+	for (var i = working_keys.length - 1; i >= 0; i--) {
+		if (working_keys[i] !== '*'){
+			if(protocol_obj[working_keys[i]]['type'] === 'array'){
+				console.log("found array: "+ working_keys[i])
+				if (protocol_obj[working_keys[i]]['map2key']){
+					var m2k_obj = protocol_obj[working_keys[i]]['map2key']
+					console.log(m2k_obj)
+					var obj_fn = m2k_obj['fn']
+					var new_parser = parse_json_top(m2k_obj['enforce'])
+					dyn_bank[proto_keys[i]] = {}
+					dyn_bank[proto_keys[i]]['parser'] = new_parser
+					var new_fn = function(x){
+						merge_dyn_keys(x.map(obj_fn),new_parser)
+					};
+					dyn_bank[proto_keys[i]]['fn'] = new_fn;
+					
+				}
+			}
+		}
+	};
 
+	console.log(dyn_bank)
+
+	// Validate protocol keys
+	if (enforcer.ck_keys(protocol_obj,OBJ_REQ_KEYS.concat(working_keys),OBJ_KEYS.concat(working_keys))){
 
 		console.log("keys: "+proto_keys)
 		console.log("reqs: "+proto_reqs)
 
 		return function(obj){
+			console.log("Started parser")
 			var state = true;
-			//Check for dynamic requirement updates
-			fn_bank_keys = Object.keys(fn_bank)
-			console.log("fn bank")
-			console.log(fn_bank)
-			if (fn_bank_keys.length > 0){
-				for (var i = fn_bank_keys.length - 1; i >= 0; i--) {
-					obj_value = obj[fn_bank_keys[i]]
-					fn_bank_function = fn_bank[fn_bank_keys[i]]
-					proto_keys = proto_keys.concat(fn_bank_function(obj_value))
-					proto_reqs = proto_reqs.concat(fn_bank_function(obj_value))
-				};
-				console.log("updated keys: "+proto_keys)
-				console.log("updated reqs: "+proto_reqs)
-			}
+
+			var dyn_bank_keys = Object.keys(dyn_bank)
+
+			for (var i = dyn_bank_keys.length - 1; i >= 0; i--) {
+				console.log("found fn bank key")
+				//run dyn_bank function based on the value of the object
+
+				//ensure we ignore optionals
+				if (obj[dyn_bank_keys[i]]){
+					dyn_bank[dyn_bank_keys[i]]['fn'](obj[dyn_bank_keys[i]]);
+				}				
+			};
+
+			console.log("keys after: "+proto_keys)
+			console.log("reqs after: "+proto_reqs)
 
 			// Check requirements
 			if (enforcer.ck_keys(obj, proto_reqs, proto_keys)){
-				//check type if not a wildcard type
+				// check type if not a wildcard type
 				if (proto_keys.length === 1 && proto_keys[0] === "*") {
 					// Do nothing
 				}
 				else {
 					console.log("KEYSS again:"+proto_keys);
+					console.log("working_keys"+working_keys)
 					for (var i = proto_keys.length - 1; i >= 0; i--) {
 						console.log("Working on: " + proto_keys[i])
 						console.log("working_key_version: " + working_keys[i])
 
 						var obj_key_type = typeof(obj[proto_keys[i]])
-						var proto_key_type = protocol_obj[working_keys[i]]['type']
+						
+						//We need a type for the object. First check if the protocol defines one. Then check if the dyn_bank has an entry
+						var proto_key_type;
+						if (protocol_obj[working_keys[i]]) {
+							proto_key_type = protocol_obj[working_keys[i]]['type']
+						}
+						else if (dyn_bank[working_keys[i]]) {
+							proto_key_type = dyn_bank[working_keys[i]]['type']
+						}
 						// console.log(typeof(obj[proto_keys[i]]),protocol_obj[working_keys[i]]['type'])
 						if ( obj_key_type !== proto_key_type ){
 							// Could be the array case as array is not a type
 							if (obj_key_type === 'object' && Array.isArray(obj[proto_keys[i]])){
 								continue;
 								//could be an optional atribute
-							} else if (obj_key_type === 'undefined' && derive_optional(proto_keys[i],proto_keys,proto_reqs)){
+							} 
+							else if (obj_key_type === 'undefined' && derive_optional(proto_keys[i],proto_keys,proto_reqs)){
 								continue;
 								//otherwise bad
-							} else{
-								console.error("Key type does not match for: "+proto_keys[i])
+							} 
+							else if(proto_key_type === "dyn_key"){
+								console.log("dyn_bank",dyn_bank)
+								state = state && dyn_bank[working_keys[i]].parser(obj[proto_keys[i]])
+							}
+							else{
+								console.error("Key type does not match for: "+proto_keys[i]+" Should be: "+ proto_key_type)
 								state = false
 							}
 							//if object generate a new parser to parse sub_object
@@ -331,81 +361,3 @@ var parser = function(){
 		gen:generate
 	}
 }()
-
-
-var enforcerproto ={
-	"name": "Enforcer.js RPC 1.0",
-	"style": "json",
-	"enforce":{
-		"keys": ["global"],
-		"require": ["global"],
-
-		"_global":{
-			"type":"object",
-			"keys":["*"],
-			"require":["keys","require"],
-			"_keys":{
-				"type":"array",
-			},
-			"_require":{
-				"type":"array"
-			}
-		}
-	}
-}
-
-var jsonrpc2proto = {
-	"name": "JSON-RPC 2.0",
-	"style": "json",
-
-	"global" : {
-		"keys": ["jsonrpc", "id"],
-		"require": ["jsonrpc", "id"],
-		"_jsonrpc": {
-			"type":"string",
-			"fixed":"2.0"
-		},
-		"_id":{
-			"type":"number",
-			"%":["trackid"]
-		}
-	},
-
-	"request": {
-		"keys": ["method", "params"],
-		"require": ["method"],
-		"_method":{
-			"type":"string"
-		},
-		"_params":{
-			"type": "object",
-			"keys": ["*"],
-			"require": [""],
-		}
-	},
-
-	"response": {
-		"keys": ["result","error"],
-		"require": [["result","error"]],
-		"result_error_mode": "split",
-		"result_key": "result",
-		"error_key": "error",
-
-		"_error":{
-			"type": "object",
-			"keys": ["code","message","data"],
-			"require": ["*"],
-			"_code": {
-				"type": "number",
-			},
-			"_message": {
-				"type": "string",
-			},
-			"_data": {
-				"type": "ANY",
-			},
-		},
-	},
-}
-
-
